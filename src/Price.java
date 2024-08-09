8451436b2154ec910b4a804ec8347bf5a7e9a19b
Emir Karamoldo <emirkaramoldo@proton.me>
2024-08-09 16:53:16 +0600

    Выполнено 1 домашнее задание

---
+++ src/Price.java
@@ -0,0 +1,17 @@
+public class Price {
+    private int price;
+    private String currency;
+
+    public Price(int price, String currency) {
+        this.price = price;
+        this.currency = currency;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+}
